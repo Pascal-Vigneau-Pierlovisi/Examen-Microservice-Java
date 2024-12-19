13800e5f202370b84bd406e215ea9375bda9015a
Pascal <20220269@webmail.univ-corse.fr>
2024-12-19 13:06:05 +0100

    Fix communication with eureka-server

--- practitioner-service/src/main/java/com/example/practitionerservice/PractitionerServiceApplication.java
+++ practitioner-service/src/main/java/com/example/practitionerservice/PractitionerServiceApplication.java
@@ -1,13 +1,31 @@
 package com.example.practitionerservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class PractitionerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PractitionerServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Liste des endpoints disponibles :");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 
 }
