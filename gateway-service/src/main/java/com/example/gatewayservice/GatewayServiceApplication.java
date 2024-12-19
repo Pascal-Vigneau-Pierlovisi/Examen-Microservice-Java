4c5131449e11b8a5bfff1bc76a11a46d01a44d46
Pascal <20220269@webmail.univ-corse.fr>
2024-12-19 14:46:18 +0100

    Bug fixes and connection of patient and practitioner services with the gateway

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,11 @@ package com.example.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +15,11 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public DefaultAddressResolverGroup addressResolverGroup() {
+        return DefaultAddressResolverGroup.INSTANCE;
     }
 
 }
